194c966d198079ea6e289a140de61e5c1f4dea2e
mrFarooque <farooqueansari85@gmail.com>
2022-09-28 18:36:57 +0530

    structure created

---
+++ TripManagementSystem/src/main/java/com/masai/TripManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripManagementSystemApplication.class, args);
+	}
+
+}
