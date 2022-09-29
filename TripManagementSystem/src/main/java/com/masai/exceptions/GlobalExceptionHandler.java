179f6cfe60feac478250d833064d4c7e596b78b0
SaurabhPagrut <saurabhpagrut2016@gmail.com>
2022-09-29 23:38:17 +0530

    added all the exception handling classes and handlers

---
+++ TripManagementSystem/src/main/java/com/masai/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,98 @@
+package com.masai.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(AdminException.class)
+	public ResponseEntity<MyErrorDetails> AdminExceptionHandler(AdminException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(BookingException.class)
+	public ResponseEntity<MyErrorDetails> BookingExceptionHandler(BookingException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(BusException.class)
+	public ResponseEntity<MyErrorDetails> BusExceptionHandler(BusException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(CustomerException.class)
+	public ResponseEntity<MyErrorDetails> CustomerExceptionHandler(CustomerException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(FeedbackException.class)
+	public ResponseEntity<MyErrorDetails> FeedbackExceptionHandler(FeedbackException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(HotelException.class)
+	public ResponseEntity<MyErrorDetails> HotelExceptionHandler(HotelException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(PackageException.class)
+	public ResponseEntity<MyErrorDetails> PackageExceptionHandler(PackageException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(ReportException.class)
+	public ResponseEntity<MyErrorDetails> ReportExceptionHandler(ReportException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(RouteException.class)
+	public ResponseEntity<MyErrorDetails> RouteExceptionHandler(RouteException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(TicketDetailsException.class)
+	public ResponseEntity<MyErrorDetails> TicketDetailsExceptionHandler(TicketDetailsException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(TravelsException.class)
+	public ResponseEntity<MyErrorDetails> TravelsExceptionHandler(TravelsException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(UserException.class)
+	public ResponseEntity<MyErrorDetails> UserExceptionHandler(UserException exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<MyErrorDetails> IllegalArgumentExceptionHandler(IllegalArgumentException exp,
+			WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<MyErrorDetails> ExceptionHandler(Exception exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), exp.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
