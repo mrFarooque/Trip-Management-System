6b44282c92174133b1fd2307946afc714fcc6bdf
SaurabhPagrut <saurabhpagrut2016@gmail.com>
2022-10-01 18:55:26 +0530

    added feedback controllers of user

--- TripManagementSystem/src/main/java/com/masai/controllers/CustomerController.java
+++ TripManagementSystem/src/main/java/com/masai/controllers/CustomerController.java
@@ -1,5 +1,7 @@
 package com.masai.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,19 +13,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.exceptions.CustomerException;
+import com.masai.exceptions.FeedbackException;
 import com.masai.exceptions.PackageException;
 import com.masai.models.Booking;
 import com.masai.models.BookingDTO;
 import com.masai.models.Customer;
 import com.masai.models.CustomerDTO;
+import com.masai.models.Feedback;
 import com.masai.service.CBookingService;
 import com.masai.service.CustomerService;
+import com.masai.service.IFeedbackService;
+import com.masai.service.IReportService;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
 	@Autowired CustomerService customerService;
 	@Autowired CBookingService bookingService;
+	@Autowired IFeedbackService feedbackService;
+	@Autowired IReportService reportService;
+	
 	
 	@PostMapping("/")
 	public ResponseEntity<Customer> saveCustomer(@RequestBody CustomerDTO customer){
@@ -36,5 +45,15 @@ public class CustomerController {
 		return new ResponseEntity<Booking>(booking,HttpStatus.CREATED);
 	}
 	
+	@PostMapping("/feedback")
+	public ResponseEntity<Feedback> createFeedback(@RequestBody Feedback feedback) throws FeedbackException{
+		Feedback savedFeedback = feedbackService.addFeedback(feedback);
+		return new ResponseEntity<Feedback>(savedFeedback, HttpStatus.CREATED);	
+	}
 	
+	@GetMapping("/feedback/{id}")
+	public ResponseEntity<List<Feedback>> getFeedbacks(@PathVariable Integer id) throws FeedbackException, CustomerException{
+		List<Feedback> feedbackList = feedbackService.findByCustomerId(id);
+		return new ResponseEntity<List<Feedback>>(feedbackList, HttpStatus.OK);
+	}
 }
