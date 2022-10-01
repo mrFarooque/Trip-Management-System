1b500225997cde777850253b5665c113e62fe292
SaurabhPagrut <saurabhpagrut2016@gmail.com>
2022-10-01 19:16:46 +0530

    added report controller by customer

--- TripManagementSystem/src/main/java/com/masai/controllers/CustomerController.java
+++ TripManagementSystem/src/main/java/com/masai/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.masai.exceptions.CustomerException;
 import com.masai.exceptions.FeedbackException;
 import com.masai.exceptions.PackageException;
+import com.masai.exceptions.ReportException;
 import com.masai.models.Booking;
 import com.masai.models.BookingDTO;
 import com.masai.models.Customer;
 import com.masai.models.CustomerDTO;
 import com.masai.models.Feedback;
+import com.masai.models.Report;
 import com.masai.service.CBookingService;
 import com.masai.service.CustomerService;
 import com.masai.service.IFeedbackService;
@@ -28,32 +31,49 @@ import com.masai.service.IReportService;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-	@Autowired CustomerService customerService;
-	@Autowired CBookingService bookingService;
-	@Autowired IFeedbackService feedbackService;
-	@Autowired IReportService reportService;
-	
-	
+	@Autowired
+	CustomerService customerService;
+	@Autowired
+	CBookingService bookingService;
+	@Autowired
+	IFeedbackService feedbackService;
+	@Autowired
+	IReportService reportService;
+
 	@PostMapping("/")
-	public ResponseEntity<Customer> saveCustomer(@RequestBody CustomerDTO customer){
-		return new ResponseEntity<Customer>(customerService.createCustomer(customer),HttpStatus.OK);
+	public ResponseEntity<Customer> saveCustomer(@RequestBody CustomerDTO customer) {
+		return new ResponseEntity<Customer>(customerService.createCustomer(customer), HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/booking/{id}")
-	public ResponseEntity<Booking> createBooking(@RequestBody BookingDTO bookingDTO,@PathVariable("id") Integer customerId) throws PackageException, CustomerException{
+	public ResponseEntity<Booking> createBooking(@RequestBody BookingDTO bookingDTO,
+			@PathVariable("id") Integer customerId) throws PackageException, CustomerException {
 		Booking booking = bookingService.createBooking(bookingDTO, customerId);
-		return new ResponseEntity<Booking>(booking,HttpStatus.CREATED);
+		return new ResponseEntity<Booking>(booking, HttpStatus.CREATED);
 	}
-	
+
 	@PostMapping("/feedback")
-	public ResponseEntity<Feedback> createFeedback(@RequestBody Feedback feedback) throws FeedbackException{
+	public ResponseEntity<Feedback> createFeedback(@RequestBody Feedback feedback) throws FeedbackException {
 		Feedback savedFeedback = feedbackService.addFeedback(feedback);
-		return new ResponseEntity<Feedback>(savedFeedback, HttpStatus.CREATED);	
+		return new ResponseEntity<Feedback>(savedFeedback, HttpStatus.CREATED);
 	}
-	
+
 	@GetMapping("/feedback/{id}")
-	public ResponseEntity<List<Feedback>> getFeedbacks(@PathVariable Integer id) throws FeedbackException, CustomerException{
+	public ResponseEntity<List<Feedback>> getFeedbacks(@PathVariable Integer id)
+			throws FeedbackException, CustomerException {
 		List<Feedback> feedbackList = feedbackService.findByCustomerId(id);
 		return new ResponseEntity<List<Feedback>>(feedbackList, HttpStatus.OK);
+	}
+
+	@PostMapping("/report")
+	public ResponseEntity<Report> createReport(@RequestBody Report report) throws ReportException {
+		Report savedReport = reportService.addReport(report);
+		return new ResponseEntity<Report>(savedReport, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/report/{id}")
+	public ResponseEntity<Report> deleteReport(@PathVariable Integer id) throws ReportException {
+		Report report = reportService.deleteReport(id);
+		return new ResponseEntity<Report>(report, HttpStatus.OK);
 	}
 }
