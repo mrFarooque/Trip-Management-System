a2f9e1a4a26e382061b4e51e97a6d2a2e2bd0df9
SaurabhPagrut <saurabhpagrut2016@gmail.com>
2022-10-01 17:53:57 +0530

    added report service interface

--- TripManagementSystem/src/main/java/com/masai/service/IReportService.java
+++ TripManagementSystem/src/main/java/com/masai/service/IReportService.java
@@ -1,5 +1,16 @@
 package com.masai.service;
 
-public interface IReportService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.masai.exceptions.ReportException;
+import com.masai.models.Report;
+
+@Service
+public interface IReportService {
+	public Report addReport(Report report)throws ReportException;
+	public Report deleteReport(Integer id)throws ReportException;
+	public Report viewReport(Integer id)throws ReportException;
+	public List<Report> viewAllReports()throws ReportException;
 }
