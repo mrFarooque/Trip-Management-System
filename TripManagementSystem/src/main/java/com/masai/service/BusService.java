b051be880642b5928196190df093689dcb715b1c
efat56 <104213039+efat56@users.noreply.github.com>
2022-10-02 14:07:55 +0530

    successfull

--- TripManagementSystem/src/main/java/com/masai/service/BusService.java
+++ TripManagementSystem/src/main/java/com/masai/service/BusService.java
@@ -5,7 +5,7 @@ import com.masai.models.Bus;
 
 public interface BusService {
 	public Bus addBus(Bus bus);
-	public Bus upDateBus(Bus bus) ;
+	public Bus upDateBus(Bus bus) throws BusException  ;
 	public Bus deleteBus(Bus bus) throws BusException;
 	
 }
